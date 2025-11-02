4408e4f9cc776c3808f189771fc368a438f07e16
espinosa-dev <alvaroespinosamontesinos@gmail.com>
2025-11-02 19:28:47 +0100

    Traga perras - Version 1.0

---
+++ src/TragaPerras.java
@@ -0,0 +1,87 @@
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TragaPerras {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int banca, dinero = 0, opcion;
+        menu();
+        do {
+            System.out.print("Elige una opción: ");
+            opcion = scn.nextInt();
+        }while (opcion != 1 && opcion != 2);
+        switch (opcion){
+            case 1:
+                menuJuego(dinero);
+                opcion = scn.nextInt();
+                switch (opcion){
+                    case 1:
+                        menuAddDinero(dinero);
+                        dinero = scn.nextInt();
+                    case 2:
+
+                        List<String> list = List.of("🍌", "🍇", "🍊", "🍋", "🍒", "🍀");
+                        Random random = new Random();
+                        String randomElement1 = list.get(random.nextInt(list.size()));
+                        System.out.print(randomElement1);
+                        String randomElement2 = list.get(random.nextInt(list.size()));
+                        System.out.print(randomElement2);
+                        String randomElement3 = list.get(random.nextInt(list.size()));
+                        System.out.print(randomElement3);
+                } break;
+            case 2:
+                System.out.println("¡Hasta pronto!");
+                return;
+            default:
+                break;
+        }
+
+
+
+    }
+    public static boolean equals(String random1, String random2, String random3) {
+        return random1.equals(random2) && random1.equals(random3);
+    }
+     public static int premio(String random1, String random2, String random3, int dinero){
+        if (equals(random1, random2, random3)){
+            dinero = 1;
+            switch (random1){
+             case "🍌":
+             case "🍇":
+             case "🍊":
+             case "🍋":
+             case "🍒":
+                 dinero *= 2; break;
+
+             case "🍀":
+                 dinero*= dinero;
+            }
+        }
+        return dinero = 0;
+     }
+     public static void menu(){
+         System.out.println("CASINO DAM");
+         System.out.println("-----------");
+         System.out.println("1. Jugar");
+         System.out.println("2. Salir");
+         System.out.println("-----------");
+
+     }
+    public static void menuJuego(int dinero){
+        System.out.println("CASINO DAM");
+        System.out.println("Dinero actual: "+dinero);
+        System.out.println("-----------");
+        System.out.println("1. Añadir dinero.");
+        System.out.println("2. Jugar");
+        System.out.println("-----------");
+
+    }
+    public static void menuAddDinero(int dinero){
+        System.out.println("CASINO DAM");
+        System.out.println("Dinero actual: "+dinero);
+        System.out.println("-----------");
+        System.out.print("Introduce el deposito que quieres realizar: ");
+
+    }
+}
